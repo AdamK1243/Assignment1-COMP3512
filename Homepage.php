9d7bc922ed702d806e493bd1753ec14c11123249
utkarsh kapoor <utkar1@Utkarsh-Macbook-Pro.local>
2023-10-12 15:08:59 -0600

    worked on homepage and single song page

---
+++ Homepage.php
@@ -0,0 +1,126 @@
+<?php
+    require_once 'Include/config.inc.php'; 
+    require_once 'Include/db-classes.inc.php';
+    
+    try{
+        $conn = Databasehelper::createConnection(array(DBCONNSTRING,DBUSER,DBPASS));
+        $s = new Homepage($conn);
+        //Using the databasehelper to run all the query since it will make this page less crowded.
+        // Top Genres
+        $topGenre = $s->getTopGenres();
+        // Top Artists
+        $topArtist = $s->getTopArtists();
+        // Most Popular Songs
+        $topSong = $s->getTopSongs();
+        // One hit wonders 
+        $oneHitWonder = $s->getOneHitWonders();
+        // acousticness
+        $acoustic = $s->getLongestAcousticSong();
+        //At the club
+        $club = $s->getAtTheClub();
+        //running
+        $run = $s->getRunning();
+        //study
+        $study = $s->getStudy();
+        
+    }catch(Exception $e){$e->getMessage();}
+?>
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Assignment1.css">
+  
+    <title>Document</title>
+    
+</head>
+
+<body>
+<header class="header">
+        <h1>COMP 3512 Assign1</h1>
+        <h3>Utkarsh Kapoor, Adam Kovas</h3>
+
+        <nav>
+            <ul>
+                <li><img src="icons/home.png" alt="home icon" /><a href="home-page.php">HOME</a></li>
+                <li><img src="icons/browse.PNG" alt="browse icon" /><a href="browse-search-result.php">BROWSE</a></li>
+                <li><img src="icons/search.PNG" alt="search icon" /><a href="search-page.php">SEARCH</a></li>
+                <li><img src="icons/browse.PNG" alt="browse/search icon" /><a href="browse-search-result.php">About Us</a></li>
+            </ul>
+        </nav>
+    
+    </header>
+        <div class="content home_container">
+            
+            <div class="homepage_grid top-genres grid 1"> 
+                <h1 class ="blur-text">Top Genres</h1>
+                <ul class= gridlist>
+                    <?php foreach($topGenre as $row){
+                            echo "<li>" . $row['genre_name'] . "</li>";}?>
+                </ul>
+            </div>
+            <div class="homepage_grid top-artist">
+                <h1 class= "blur-text">Top Artists</h1>
+                <ul>
+                <?php foreach($topArtist as $row){
+                        echo "<li>" . $row['artist_name'] . "</li>";}?>     
+                </ul>
+            </div>
+            <div class="homepage_grid top-songs">
+                <h1 class="blur-text">Top Songs</h1>
+                <ul>
+                <?php foreach($topSong as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul>
+            </div>
+            <div class="homepage_grid one-hit">
+                <h1 class="blur-text">One Hit Wonders</h1>
+                <ul>
+                <?php foreach($oneHitWonder as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul>
+            </div>
+            <div class="homepage_grid long ">
+                <h1 class="blur-text">Longest Acoustic Song</h1>
+                <ul>
+                <?php foreach($acoustic as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul>
+            </div>
+            <div class="homepage_grid club">
+                <h1 class="blur-text">At The Club</h1>
+                <ul>
+                <?php foreach($club as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul>
+            </div>
+            <div class="homepage_grid run">
+                <h1 class="blur-text">Running songs</h1>
+                <ul>
+                <?php foreach($run as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul> 
+            </div>
+            <div class="homepage_grid study">
+                <h1 class="blur-text">Study Songs</h1>
+                <ul>
+                <?php foreach($study as $row){
+                        echo "<li><a href='SongInfo.php?curr=si&songID=" . $row['song_id'] . "'>" . $row['title'] . '</a> By ' . $row['artist_name'] . "</li>";}?>
+                </ul>
+            </div>
+        </div>
+        
+   
+     <footer>
+        <h4>COMP 3512</h4>
+        <p id="copyright">©Kapoor, Kovas</p>
+        <div class="Github">
+            <li><a href="https://github.com/AdamK1243/Assignment1-COMP3512"><img src= "icons/git.png" alt= "git icon"/></a></li>
+            <a href="https://github.com/Spencerreids">Spencer's github</a>
+            <a href="https://github.com/mlind423">Matthew's github</a>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ single-song.php
@@ -0,0 +1,117 @@
+<?php 
+require_once('Include/config.inc.php'); 
+require_once('Include/db-classes.inc.php');
+try{
+    $conn = Databasehelper::createConnection(array(DBCONNSTRING,DBUSER,DBPASS));
+    $musicGateway = new MusicDB($conn);
+    
+    if(!empty($_GET['songID'])){
+        $song = $musicGateway->getSong($_GET['songID']);
+      }else{
+        $song = $musicGateway->getSong(1001);//will be set to the first song in the database unless otherwise specified
+      }
+      $conn = null;
+    }catch(Exception $e){$e->getMessage();}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Assignment1.css">
+    <title>Song Info</title>
+</head>
+
+<body>
+<header class="header">
+        <h1>COMP 3512 Assign1</h1>
+        <h3>Utkarsh Kapoor, Adam Kovas</h3>
+
+        <nav>
+            <ul>
+                <li><img src="icons/home.png" alt="home icon" /><a href="home-page.php">HOME</a></li>
+                <li><img src="icons/browse.PNG" alt="browse icon" /><a href="browse-search-result.php">BROWSE</a></li>
+                <li><img src="icons/search.PNG" alt="search icon" /><a href="search-page.php">SEARCH</a></li>
+                <li><img src="icons/browse.PNG" alt="browse/search icon" /><a href="browse-search-result.php">About Us</a></li>
+            </ul>
+        </nav>
+    
+    </header>
+
+
+</div>
+<div class="content">
+        <table class="songInfo"> 
+            <tr>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Artist Type</th>
+                <th>Genre</th>
+                <th>Year</th>
+                <th>Length</th>
+            </tr>
+                <?php 
+                foreach($song as $row){ 
+                    $length = $row['duration'];
+                    $minutes = number_format($length/60, 0);
+                    $seconds = number_format($length%60);
+                    if ($seconds < 10){
+                        $seconds = "0$seconds";
+                    }
+                    echo "<tr>"; 
+                    echo "<td>" . $row['title']  . ' </td>'; 
+                    echo "<td>" . $row['artist_name']  . ' </td>';  
+                    echo "<td>" . $row['type_name']  . ' </td>';
+                    echo "<td>" . $row['genre_name']  . ' </td>'; 
+                    echo "<td>" . $row['year']  . ' </td>'; 
+                    echo "<td>" . $minutes . ":"  . $seconds . ' </td>';  
+                    echo "</tr>";
+                }
+                ?>
+                
+        </table>
+
+
+ <div class="progress">
+                <?php foreach($song as $row){?> 
+                    <div class="progress-bars">
+                    <label for="bpm">Beats per minute</label></br>
+                    Low <progress min="0" max="300" value="<?= $row['bpm']?>" id="bpm"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Energy</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['energy']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Danceability</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['danceability']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Liveness</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['liveness']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Valence</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['valence']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Acousticness</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['acousticness']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Speechiness</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['speechiness']?>"></progress> High</br>
+                    </div>
+                    <div class="bars">
+                    <label>Popularity</label></br>
+                    Low <progress min="0" max="100" value="<?= $row['popularity']?>"></progress> High</br>
+                    </div>
+                <?php }?>
+      </div>
+ </div>     
+    <?php include('include/Footer.php')?>
+
+</body>
+</html>
